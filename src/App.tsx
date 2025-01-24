4f871d95eafcb3e04c2de3801a6560c353996e24
Lil-papa <candy1571@gmail.com>
2025-01-24 15:26:32 +0300

    add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import './Index.css';
+import React from 'react';
+
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+};
+
 function App() {
-  return <div>hello world</div>;
+  const [tasks, setTasks] = React.useState<Task[]>([
+    { id: 1, title: 'Тестовое задание', isCompleted: false },
+  ]);
+
+  const [taskName, setTaskName] = React.useState('');
+
+  const onAddTask = () => {
+    setTasks([
+      ...tasks,
+      { id: Date.now(), title: taskName, isCompleted: false },
+    ]);
+  };
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <label htmlFor="task-input" className="sr-only">
+        Add Task:
+      </label>
+      <input
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+        id="task-input"
+      />
+      <button onClick={onAddTask}>Add Task</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
